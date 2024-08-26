fe2ed56247de79a984b184fb8205080cf1534e46
JahdulIslam <ovi242-35-019@diu.edu.bd>
2024-08-26 11:48:54 +0600

    Update and rename main.c to variables.c

--- variables.c
+++ variables.c
@@ -10,9 +10,15 @@ int main(){
     //"float" takes decimal values of 6-7 digits and takes up 4 bytes of space (%f)
         float gpa=4.83;
     //"double" takes decimal values of 15 digits and takes up 8 bytes of space (%lf)
+         double coffeeCost= 19.99;
+    //We can store scientific values in "float" or "double" in the following way:
+        const float massEarth=6e24;
         printf("My name is \"%s\" \n", name);
         printf("I am %d years old \n", age);
         printf("I am in \"%c\" section \n", section);
         printf("My gpa was %.2f \n", gpa);
+        printf("The cost of coffee at campus is %.2lf \n", coffeeCost);
+        printf("The mass of Earth is %f \n", massEarth);
+        printf("%lu", sizeof(coffeeCost)); //This is the way to check the space occupied by a variable
     return 0;
 }
