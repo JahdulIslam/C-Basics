3f3e1e15aa7182eaa7cee8d62292048acdd209cd
JahdulIslam <ovi242-35-019@diu.edu.bd>
2024-08-26 17:40:05 +0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+        bool isFishTasty = true;
+        bool isStrawberrySweet = false;
+        printf("%d \n", isFishTasty);
+        printf("%d \n", isStrawberrySweet);
+        printf("%d", isFishTasty == isStrawberrySweet);
+    return 0;
+}
