4084964bd38e8b88f1c348db06ae4e85db8ea713
JahdulIslam <ovi242-35-019@diu.edu.bd>
2024-08-26 16:08:25 +0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+        //difference between prefix (++i) and postfix (i++)
+        /*int x, i;
+        i = 0;
+        x = i++;
+        printf("%d", x); output 0*/
+        int x, i;
+        i = 0;
+        x = ++i;
+        printf("%d", x);//output 1
+    return 0;
+}
